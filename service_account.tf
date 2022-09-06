cd824869a57607cbdf3f6b7dcbd8c61756f5623c
Oğul Tütüncü <41284115+WickedBast@users.noreply.github.com>
2022-09-06 19:12:01 +0200

    Updates on service_account.tf

--- service_account.tf
+++ service_account.tf
@@ -3,10 +3,6 @@ resource "google_service_account" "cloudsql-proxy-terraform" {
   display_name = "cloudsql-proxy-terraform"
 }
 
-data "google_service_account" "cloudsql-proxy-terraform" {
-  account_id = "cloudsql-proxy-terraform"
-}
-
 resource "google_project_iam_binding" "cloudsql-proxy-binding" {
   project = var.project
   role    = "roles/cloudsql.client"
